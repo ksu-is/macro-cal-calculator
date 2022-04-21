b4c17fa7d5be06ca8b7e73a162d354e10dbc7e86
lindseydoan <ldoan1@students.kennesaw.edu>
2022-04-21 19:18:45 -0400

    changes made to code

--- calorie_calc.py
+++ calorie_calc.py
@@ -34,7 +34,7 @@ def calculate_activity(bmr_result):
     return(int(activity_level))
 
 def gain_or_lose(activity_level):
-    goals = input('Do you want to lose, maintain, or gain weight: ')
+    goals = input('Do you want to lose fat, maintain weight, or gain weight: ')
 
     if goals == 'lose':
         calories = activity_level - 500
